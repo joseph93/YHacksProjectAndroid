d47835b8bfb5003cacbc0dfcc16e3d3f1613b1af
joseph <joseph.atallah93@gmail.com>
2016-11-12 17:55:12 -0500

    Added the login to facebook functionality

--- app/src/main/java/com/example/josep/yhacks/MainActivity.java
+++ app/src/main/java/com/example/josep/yhacks/MainActivity.java
@@ -1,30 +1,21 @@
 package com.example.josep.yhacks;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-
-        Button login_button = (Button)findViewById(R.id.button2);
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToHomePage();
-            }
-        });
-    }
-
-    private void goToHomePage() {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+        Fragment fragment = new MainFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_frame, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
     }
 }
---
+++ app/src/main/java/com/example/josep/yhacks/MyApplication.java
@@ -0,0 +1,45 @@
+package com.example.josep.yhacks;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by josep on 11/12/2016.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.josep.yhacks",
+                    PackageManager.GET_SIGNATURES);
+            for(Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("JOE:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        }
+
+        catch (PackageManager.NameNotFoundException e) {
+
+        }
+
+        catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
